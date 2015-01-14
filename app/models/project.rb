f8687f78c82d881ec183a49063882c0ae298408e
Andrew Fyfe <enumera2000@yahoo.co.uk>
2015-01-14 22:30:23 +0000

    dependent destroy implement on project and goals to destroy tasks time records will remain intact

--- app/models/project.rb
+++ app/models/project.rb
@@ -1,8 +1,8 @@
 class Project < ActiveRecord::Base
   attr_accessible :name, :goal_ids, :user_ids, :trello_project_id, :no_of_goals, :task_ids
 
-  has_many :goals
-  has_many :tasks
+  has_many :goals, dependent: :destroy
+  has_many :tasks, dependent: :destroy
 
   has_and_belongs_to_many :users
 end
