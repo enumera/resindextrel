c098036c21a44ca065f52a82779fbb4e5d6251fd
Andrew Fyfe <enumera2000@yahoo.co.uk>
2015-01-19 20:33:36 +0000

    updated seed for all current users

--- db/seeds.rb
+++ db/seeds.rb
@@ -35,6 +35,61 @@ Importance.create(name:"Should do", importance_ref: 3, importance_value: 3, user
 Importance.create(name:"Someone/I really needs this", importance_ref: 4, importance_value: 2, user_id: 1, name_status: "active" )
 Importance.create(name:"Someone/I REALLY needs this", importance_ref: 5, importance_value: 1, user_id: 1, name_status: "active" )
 
+Difficulty.create(name:"Easy-done it before", difficulty_ref: 1, difficulty_value: 1, user_id: 2, name_status: "active" )
+Difficulty.create(name:"Something slightly different", difficulty_ref: 2, difficulty_value: 2, user_id: 2, name_status: "active" )
+Difficulty.create(name:"Tricky", difficulty_ref: 3, difficulty_value: 3, user_id: 1, name_status: "active" )
+Difficulty.create(name:"Really difficult", difficulty_ref: 4, difficulty_value: 4, user_id: 2, name_status: "active" )
+Difficulty.create(name:"Mission Impossible!", difficulty_ref: 5, difficulty_value: 5, user_id: 2, name_status: "active" )
+
+Importance.create(name:"Would do", importance_ref: 1, importance_value: 5, user_id: 2, name_status: "active" )
+Importance.create(name:"Could do", importance_ref: 2, importance_value: 4, user_id: 2, name_status: "active" )
+Importance.create(name:"Should do", importance_ref: 3, importance_value: 3, user_id: 2, name_status: "active" )
+Importance.create(name:"Someone/I really needs this", importance_ref: 4, importance_value: 2, user_id: 2, name_status: "active" )
+Importance.create(name:"Someone/I REALLY needs this", importance_ref: 5, importance_value: 1, user_id: 2, name_status: "active" )
+
+
+Difficulty.create(name:"Easy-done it before", difficulty_ref: 1, difficulty_value: 1, user_id: 3, name_status: "active" )
+Difficulty.create(name:"Something slightly different", difficulty_ref: 2, difficulty_value: 3, user_id: 2, name_status: "active" )
+Difficulty.create(name:"Tricky", difficulty_ref: 3, difficulty_value: 3, user_id: 3, name_status: "active" )
+Difficulty.create(name:"Really difficult", difficulty_ref: 4, difficulty_value: 4, user_id: 3, name_status: "active" )
+Difficulty.create(name:"Mission Impossible!", difficulty_ref: 5, difficulty_value: 5, user_id: 3, name_status: "active" )
+
+Importance.create(name:"Would do", importance_ref: 1, importance_value: 5, user_id: 3, name_status: "active" )
+Importance.create(name:"Could do", importance_ref: 2, importance_value: 4, user_id: 3, name_status: "active" )
+Importance.create(name:"Should do", importance_ref: 3, importance_value: 3, user_id: 3, name_status: "active" )
+Importance.create(name:"Someone/I really needs this", importance_ref: 4, importance_value: 2, user_id: 3, name_status: "active" )
+Importance.create(name:"Someone/I REALLY needs this", importance_ref: 5, importance_value: 1, user_id: 3, name_status: "active" )
+
+
+Difficulty.create(name:"Easy-done it before", difficulty_ref: 1, difficulty_value: 1, user_id: 4, name_status: "active" )
+Difficulty.create(name:"Something slightly different", difficulty_ref: 2, difficulty_value: 2, user_id: 2, name_status: "active" )
+Difficulty.create(name:"Tricky", difficulty_ref: 3, difficulty_value: 3, user_id: 4, name_status: "active" )
+Difficulty.create(name:"Really difficult", difficulty_ref: 4, difficulty_value: 4, user_id: 4, name_status: "active" )
+Difficulty.create(name:"Mission Impossible!", difficulty_ref: 5, difficulty_value: 5, user_id: 4, name_status: "active" )
+
+Importance.create(name:"Would do", importance_ref: 1, importance_value: 5, user_id: 4, name_status: "active" )
+Importance.create(name:"Could do", importance_ref: 2, importance_value: 4, user_id: 4, name_status: "active" )
+Importance.create(name:"Should do", importance_ref: 3, importance_value: 3, user_id: 4, name_status: "active" )
+Importance.create(name:"Someone/I really needs this", importance_ref: 4, importance_value: 2, user_id: 4, name_status: "active" )
+Importance.create(name:"Someone/I REALLY needs this", importance_ref: 5, importance_value: 1, user_id: 4, name_status: "active" )
+
+Difficulty.create(name:"Easy-done it before", difficulty_ref: 1, difficulty_value: 1, user_id: 5, name_status: "active" )
+Difficulty.create(name:"Something slightly different", difficulty_ref: 2, difficulty_value: 2, user_id: 5, name_status: "active" )
+Difficulty.create(name:"Tricky", difficulty_ref: 3, difficulty_value: 3, user_id: 1, name_status: "active" )
+Difficulty.create(name:"Really difficult", difficulty_ref: 4, difficulty_value: 4, user_id: 5, name_status: "active" )
+Difficulty.create(name:"Mission Impossible!", difficulty_ref: 5, difficulty_value: 5, user_id: 5, name_status: "active" )
+
+Importance.create(name:"Would do", importance_ref: 1, importance_value: 5, user_id: 6, name_status: "active" )
+Importance.create(name:"Could do", importance_ref: 2, importance_value: 4, user_id: 6, name_status: "active" )
+Importance.create(name:"Should do", importance_ref: 3, importance_value: 3, user_id: 6, name_status: "active" )
+Importance.create(name:"Someone/I really needs this", importance_ref: 4, importance_value: 2, user_id: 6, name_status: "active" )
+Importance.create(name:"Someone/I REALLY needs this", importance_ref: 5, importance_value: 1, user_id: 6, name_status: "active" )
+
+Importance.create(name:"Would do", importance_ref: 1, importance_value: 5, user_id: 6, name_status: "active" )
+Importance.create(name:"Could do", importance_ref: 2, importance_value: 4, user_id: 6, name_status: "active" )
+Importance.create(name:"Should do", importance_ref: 3, importance_value: 3, user_id: 6, name_status: "active" )
+Importance.create(name:"Someone/I really needs this", importance_ref: 4, importance_value: 2, user_id: 6, name_status: "active" )
+Importance.create(name:"Someone/I REALLY needs this", importance_ref: 5, importance_value: 1, user_id: 6, name_status: "active" )
 
 
 
