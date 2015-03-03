226052683a98cff31fe57afff1b4ee753b0af0f5
Andrew Fyfe <enumera2000@yahoo.co.uk>
2015-03-03 23:24:05 +0000

    set the environment variables up properly

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -75,8 +75,8 @@ Trelloindex::Application.configure do
     config.action_mailer.smtp_settings = {
       :address   => "smtp.mandrillapp.com",
       :port      => 25,
-      :user_name => "MANDRILL_USERNAME",
-      :password  => "MANDRILL_API_KEY"
+      :user_name => ENV["MANDRILL_USERNAME"],
+      :password  => ENV["MANDRILL_API_KEY"]
 
     }
 
